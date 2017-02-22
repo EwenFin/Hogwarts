21cc272fa99d90563059578c67f1714348c3303b
Ewen Carr <ewencarr@hotmail.com>
2017-02-22 15:49:16 +0000

    database working (again)

--- db/seed.rb
+++ db/seed.rb
@@ -1,32 +1,61 @@
 require_relative('../models/student')
+require_relative('../models/house')
 require('pry')
 
  Student.delete_all
+ House.delete_all
 
 student1 = Student.new({
   "first_name" => "Harry",
   "second_name" => "Potter",
-  "house" => "Gryffindor",
+  "house" => "1",
   "age" => 13
 })
 
 student2 = Student.new({
   "first_name" => "Ron",
   "second_name" => "Weasely",
-  "house" => "Griffindor",
+  "house" => "2",
   "age" => 12
 })
 
 student3 = Student.new({
   "first_name" => "Draco",
   "second_name" => "Malfoy",
-  "house" => "Slytherin",
+  "house" => "4",
   "age" => 14
 })
 
 student1.save
 student2.save
 student3.save
+
+house1 = House.new({
+  "name" => "Gryffindor",
+  "logo_url" => "https://tinyurl.com/jg344va"
+  })
+
+house2 = House.new({
+  "name" => "Ravenclaw",
+  "logo_url" => "www.facebook.com"
+  })
+
+house3 = House.new({
+  "name" => "Hufflepuff",
+  "logo_url" => 'www.twitter.com'
+  })
+
+house4 = House.new({
+  "name" => "Slytherin",
+  "logo_url" => 'www.wikipedia.org'
+  })
+
+house1.save
+house2.save
+house3.save
+house4.save
+
+
 
 binding.pry
 nil
---
+++ models/house.rb
@@ -0,0 +1,22 @@
+require_relative('../db/sqlrunner.rb')
+
+class House
+  attr_accessor :name, :logo_url
+  attr_reader :id
+  def initialize(options)
+    @id = options['id'].to_i
+    @name = options['name']
+    @logo_url = options['logo_url']
+  end
+
+  def self.delete_all
+    sql = "DELETE FROM houses"
+    SqlRunner.run(sql)
+  end
+
+  def save
+    sql = "INSERT INTO houses (name, logo_url) VALUES ('#{@name}', '#{@logo_url}') RETURNING id"
+    house = SqlRunner.run(sql).first
+    @id= house['id'].to_i
+  end
+end
